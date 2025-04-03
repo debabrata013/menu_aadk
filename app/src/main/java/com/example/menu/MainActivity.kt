6792a17d9ce90f10805d2f9953eccc6549b43741
DEV <pattnaikd833@gmail.com>
2025-04-03 19:12:06 +0530

    Add MediaRouter library and adjust main activity
    - Added the `androidx.mediarouter:mediarouter:1.7.0` dependency in `libs.versions.toml`.
    - Updated `MainActivity.kt` to use view binding with `ActivityMainBinding`.
    - Created `onCreateOptionsMenu` in `MainActivity.kt` for the options menu.

--- app/src/main/java/com/example/menu/MainActivity.kt
+++ app/src/main/java/com/example/menu/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.menu
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.menu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+       binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+//        return super.onCreateOptionsMenu(menu)
     }
 }
