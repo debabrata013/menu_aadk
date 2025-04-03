a43b76c1230564b00039afec978b2c48329199d4
DEV <pattnaikd833@gmail.com>
2025-04-03 19:23:28 +0530

    Implement menu item click handling
    - Added `onOptionsItemSelected` in `MainActivity.kt` to handle clicks on menu items.
    - Implemented logic to display a toast message when "Edit" or "Settings" menu items are clicked.

--- app/src/main/java/com/example/menu/MainActivity.kt
+++ app/src/main/java/com/example/menu/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.menu
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -25,5 +27,23 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.option_menu, menu)
         return true
 //        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item!!.itemId) {
+            R.id.edit -> {
+                // Handle edit action
+                Toast.makeText(this, "Edit Clicked", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.settings -> {
+                // Handle settings action
+                Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show()
+                return true
+            }
+                // Handle settings action
+
+        }
+       return true
     }
 }
